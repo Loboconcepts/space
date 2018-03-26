2eab001a31cad8ee659e17645619ca8b05a5a5c3
Loboconcepts <adam@loboconcepts.com>
2018-03-26 17:26:53 -0400

    Update world.js

--- world.js
+++ world.js
@@ -1,7 +1,37 @@
-var worldArray = "000000000000000000000100000000000000000000100000000000000000000000010000000000000000000000001000000000000000000000000100000000000000000000000000000010"
+var worldArray = [];
+var orientation = "BACK";
+var x = 5;
 
-function world() {
-	console.log(worldArray.length);
+
+function generateWorld() {
+	for (i=0;i<125;i++) {
+		if (worldArray.length%6==0) {
+			worldArray.push(1);	
+		}
+		else {
+			worldArray.push(0);	
+		}
+		
+	}
 }
 
-world();
+function fieldOfView() {
+	if (orientation == "BACK") {
+		tableView(0,0,"1");
+		tableView(0,1,"2");
+		tableView(0,2,"3");
+		tableView(1,0,"4");
+		tableView(1,1,"5");
+		tableView(1,2,"6");
+		tableView(2,0,"7");
+		tableView(2,1,"SHIP BACK");
+		tableView(2,2,"9");
+	}
+}
+
+function tableView(tr,td,isWhat) {
+	document.querySelector("#fieldOfView").childNodes[0].childNodes[tr].childNodes[td].innerHTML = isWhat;
+}
+
+generateWorld();
+fieldOfView();
