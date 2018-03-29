23b78a7c081b9dc27cda8d0106bbc0a91ec6b3fe
loboconcepts <adam@loboconcepts.com>
2018-03-28 22:11:30 -0400

    finally

--- world.js
+++ world.js
@@ -1,15 +1,13 @@
 var worldArray = [];
-var orientation = "BACK";
+var i = 0;
 var x = 3;
-var y = 1;
-var z = x;
+var y = 9;
+var z = 1;
 var currentLocation = 23;
+var orientation = "BACK"
 var directionArray = ["N","E","S","W","UP","DOWN"];
 var direction = directionArray[0];
 var topfacing = directionArray[4];
-var fieldOfViewBack = [currentLocation-(x*x)*2-y,currentLocation-(x*x)*2,currentLocation-(x*x)*2+y,currentLocation-(x*x)-y,currentLocation-(x*x),currentLocation-(x*x)+y,currentLocation-y,currentLocation,currentLocation+y];
-var fieldOfViewFront = [currentLocation-(z*2)-1,currentLocation-(z*2),currentLocation-(z*2)+1,currentLocation+(x/z)-1,currentLocation+(x/z),currentLocation+(x/z)-z,currentLocation-(x/z),currentLocation,currentLocation+(x/z)];
-var currentMoves = [];
 
 ////             y+ /z+
 ////             | /
@@ -21,72 +19,149 @@ var currentMoves = [];
 
 
 
-document.querySelector("#compass").innerHTML = "Direction: " + direction + "<br>Top Facing: " + topfacing;
 
-function fieldOfViewBasedOnDirection() {
+
+function cubeShipPositioning(direction, topfacing, pos, orient) {
+	// upward movement UM || forward movement FM || lateral movement LM
+	var um;
+	var fm;
+	var lm;
+
 	switch (direction) {
-	case "N":
-		y = 1;
-		z = x;
-		break;
-	case "E":
-		y = x;
-		z = 1;
-		break;
-	case "S":
-		y = -1;
-		z = -x;
-		break;
-	case "W":
-		y = -x;
-		z = 1;
-		break;
-	default:
-		console.alert("Error");
+		case "N":
+			fm = -x;
+			break;
+		case "S":
+			fm = x;
+			break;
+		case "E":
+			fm = z;
+			break;
+		case "W":
+			fm = -z;
+			break;
+		case "UP":
+			fm = -y;
+			break;
+		case "down":
+			fm = y;
+			break;
+		default:
+			console.log("error");
+	};
+	switch (topfacing) {
+		case "N":
+			um = -x;
+			break;
+		case "S":
+			um = x;
+			break;
+		case "E":
+			um = z;
+			break;
+		case "W":
+			um = -z;
+			break;
+		case "UP":
+			um = -y;
+			break;
+		case "down":
+			um = y;
+			break;
+		default:
+			console.log("error");
+	};
+	switch (Math.abs(fm) + Math.abs(um)) {
+		case 12:
+			if (fm*um < 0) {
+				lm = -z;
+			}
+			else {
+				lm = z;
+			}
+			break;
+		case 10:
+			if (fm*um < 0) {
+				lm = x;
+			}
+			else {
+				lm = -x;
+			}
+			break;
+		case 4:
+			if (fm*um < 0) {
+				lm = y;
+			}
+			else {
+				lm = -y;
+			}
+			break;
+		default:
+			console.log("error");
 	}
-	fieldOfViewBack = [currentLocation-(x*x)*2-y,currentLocation-(x*x)*2,currentLocation-(x*x)*2+y,currentLocation-(x*x)-y,currentLocation-(x*x),currentLocation-(x*x)+y,currentLocation-y,currentLocation,currentLocation+y];
-	fieldOfViewFront = [currentLocation-(z*2)-1,currentLocation-(z*2),currentLocation-(z*2)+1,currentLocation+(x/z)-1,currentLocation+(x/z),currentLocation+(x/z)-z,currentLocation-(x/z),currentLocation,currentLocation+(x/z)];
+	if (orient == "BACK") {
+		tableView(0,0,(pos + 2*um - lm));
+		tableView(0,1,(pos + 2*um));
+		tableView(0,2,(pos + 2*um + lm));
+		tableView(1,0,(pos + um - lm));
+		tableView(1,1,(pos + um)); 
+		tableView(1,2,(pos + um + lm));
+		tableView(2,0,(pos - lm));
+		tableView(2,1,"&#8743;<br>&#9669;&#9677;&#9677;&#9677;&#9659;"); 
+		tableView(2,2,(pos + lm)); 
+	}
+	if (orient == "TOP") {
+		tableView(0,0,(pos + 2*fm - lm)); //19
+		tableView(0,1,(pos + 2*fm)); //20
+		tableView(0,2,(pos + 2*fm + lm)); //21
+		tableView(1,0,(pos + fm - lm)); //22
+		tableView(1,1,(pos + fm)); //23
+		tableView(1,2,(pos + fm + lm)); //24
+		tableView(2,0,(pos - lm)); //25
+		tableView(2,1,"&#9651;<br>&#8834;&#8890;&#8835;<br>&#9677;&#9677;&#9677;"); //26
+		tableView(2,2,(pos + lm)); //27
+	}
+	document.querySelector("#compass").innerHTML = "Direction: " + direction + "<br>Top Facing: " + topfacing;
 }
+
+cubeShipPositioning(direction,"UP", 23, orientation);
 
 function yaw(lr) {
 	if (lr == "left") {
 		switch (direction) {
 		case "N":
-			direction = directionArray[3];
+			direction = "W";
 			break;
 		case "E":
-			direction = directionArray[0];
+			direction = "N";
 			break;
 		case "S":
-			direction = directionArray[1];
+			direction = "E";
 			break;
 		case "W":
-			direction = directionArray[2];
+			direction = "S";
 			break;
 		}
 	}
 	if (lr == "right") {
 		switch (direction) {
 		case "N":
-			direction = directionArray[1];
+			direction = "E";
 			break;
 		case "E":
-			direction = directionArray[2];
+			direction = "S";
 			break;
 		case "S":
-			direction = directionArray[3];
+			direction = "W";
 			break;
 		case "W":
-			direction = directionArray[0];
+			direction = "N";
 			break;
 		}
 	}
 	document.querySelector("#compass").innerHTML = "Direction: " + direction + "<br>Top Facing: " + topfacing;
-	fieldOfViewBasedOnDirection();
-	fieldOfView();
+	cubeShipPositioning(direction,"UP", 23, orientation);
 }
-
-fieldOfViewBasedOnDirection()
 
 function generateWorld() {
 	for (i=0;i<(x*x*x);i++) {
@@ -102,36 +177,11 @@ function generateWorld() {
 function switchOrientation() {
 	if (orientation == "BACK") {
 		orientation = "TOP";
-		fieldOfView();
+		cubeShipPositioning(direction,"UP", 23, orientation);
 	}
 	else {
 		orientation = "BACK";
-		fieldOfView();
-	}
-}
-
-function fieldOfView() {
-	if (orientation == "BACK") {
-		tableView(0,0,fieldOfViewBack[0]); //7
-		tableView(0,1,fieldOfViewBack[1]); //8
-		tableView(0,2,fieldOfViewBack[2]); //9
-		tableView(1,0,fieldOfViewBack[3]); //16
-		tableView(1,1,fieldOfViewBack[4]); //17
-		tableView(1,2,fieldOfViewBack[5]); //18
-		tableView(2,0,fieldOfViewBack[6]); //25
-		tableView(2,1,"&#8743;<br>&#9669;&#9677;&#9677;&#9677;&#9659;"); //26
-		tableView(2,2,fieldOfViewBack[8]); //27
-	}
-	if (orientation == "TOP") {
-		tableView(0,0,fieldOfViewFront[0]); //19
-		tableView(0,1,fieldOfViewFront[1]); //20
-		tableView(0,2,fieldOfViewFront[2]); //21
-		tableView(1,0,fieldOfViewFront[3]); //22
-		tableView(1,1,fieldOfViewFront[4]); //23
-		tableView(1,2,fieldOfViewFront[5]); //24
-		tableView(2,0,fieldOfViewFront[6]); //25
-		tableView(2,1,"&#9651;<br>&#8834;&#8890;&#8835;<br>&#9677;&#9677;&#9677;"); //26
-		tableView(2,2,fieldOfViewFront[8]); //27
+		cubeShipPositioning(direction,"UP", 23, orientation);
 	}
 }
 
@@ -140,4 +190,3 @@ function tableView(tr,td,isWhat) {
 }
 
 generateWorld();
-fieldOfView();
