497383623a226e351756dc377eedf1a6a8c61a34
loboconcepts <adam@loboconcepts.com>
2018-03-28 22:43:50 -0400

    tableview error?

--- world.js
+++ world.js
@@ -100,7 +100,8 @@ function cubeShipPositioning(direction, topfacing, pos, orient) {
 			console.log("error");
 	}
 	if (orient == "BACK") {
-		tableView("t0-0",(pos + 2*um - lm));
+		document.querySelector("#t0-0").innerHTML = (pos + 2*um - lm);
+		// tableView("t0-0",(pos + 2*um - lm));
 		tableView("t0-1",(pos + 2*um));
 		tableView("t0-2",(pos + 2*um + lm));
 		tableView("t1-0",(pos + um - lm));
