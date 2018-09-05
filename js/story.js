2d2e6045a5be42fe488e2d2ebde5302010caea94
Adam <adam@loboconcepts.com>
2018-09-05 12:26:03 -0400

    ch

--- js/story.js
+++ js/story.js
@@ -7,6 +7,7 @@ memory.style.height = window.innerHeight + "px";
 
 
 function beginStory(howManyChapters){
+	if (!howManyChapters) {howManyChapters = 0};
 	clearInterval(stalled);
 	var c = 1;
 	disableButtons(true);
@@ -28,31 +29,28 @@ function beginStory(howManyChapters){
 	storyChapter = storyChapter+1;
 	document.getElementById("memory").addEventListener("keyup", function(event) {event.preventDefault();
 		console.log(event.keyCode);
-		if (event.keyCode === 13) {
-			if (storyChapter <= howManyChapters) {
-				document.getElementById("memory").innerHTML += "<p style='color:rgba(0,0,0,0);' id='sc" + storyChapter + "'>" + story[storyChapter] + "</p>";
-				var animation = setInterval(function() {
-					console.log(c);
-					c = c+1;
-					if (c <= 100) {
-						document.getElementById("memory").lastChild.style.color = "rgba(0,0,0,"+ c/100 +")";
-					};
-				  if (c > 100) clearInterval(animation);
-				}, 15);
-				c=1;
-				storyChapter = storyChapter+1;
-			}
-			else {
-				document.getElementById("memory").innerHTML = "";
-				memory.style.display = "none";
-				memory.style.backgroundColor = "rgba(255,255,255,0)";
-				storyTime = false;
-				disableButtons(false);
-				generalState();
-			};
+		if (storyChapter <= howManyChapters) {
+			document.getElementById("memory").innerHTML += "<p style='color:rgba(0,0,0,0);' id='sc" + storyChapter + "'>" + story[storyChapter] + "</p>";
+			var animation = setInterval(function() {
+				console.log(c);
+				c = c+1;
+				if (c <= 100) {
+					document.getElementById("memory").lastChild.style.color = "rgba(0,0,0,"+ c/100 +")";
+				};
+			  if (c > 100) clearInterval(animation);
+			}, 15);
+			c=1;
+			storyChapter = storyChapter+1;
+		}
+		else {
+			document.getElementById("memory").innerHTML = "";
+			memory.style.display = "none";
+			memory.style.backgroundColor = "rgba(255,255,255,0)";
+			storyTime = false;
+			disableButtons(false);
+			generalState();
 		};
 	});
-	return console.log("Yay done!");
 };
 
 // canvas.style.left = (window.innerWidth * 0.5 - W * scale * 0.5) + "px";
